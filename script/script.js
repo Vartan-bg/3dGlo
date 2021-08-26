29ab4768a761cd33f354716fb9a8e031cf104c26
Vartan Bagdasarov <vartanbagdasarov@mail.ru>
2021-08-26 21:40:51 +0300

    Выполнил задание 23

--- script/script.js
+++ script/script.js
@@ -294,4 +294,59 @@ window.addEventListener('DOMContentLoaded', function () {
     };
     slider();
 
+
+    //отслеживание ввода в инпуты
+    const inputsСhange = () => {
+        const allInputs = [...document.getElementsByTagName('input')];
+        allInputs.forEach((elem) => {
+            elem.addEventListener('blur', () => { 
+            //инпут "Ваше имя" - только кирилица и запятые, первая буква каждого слова - заглавная, удалены все лишние пробелы и точки
+                if (elem.getAttribute('name') === 'user_name') {
+                        elem.value = elem.value.replace(/ {2,}/g, ' ');
+                        elem.value = elem.value.replace(/-{2,}/g, '-');
+                        elem.value = elem.value.replace(/(-$|^-)/g, '-');
+                        elem.value = elem.value.replace(/([а-я-]+) ([А-я-]+)/ig, (val1) => 
+                            val1[0].toUpperCase() + val1.slice(1).toLowerCase());
+                        elem.value = elem.value.replace(/(^(?:^|^\s))./gi, (match) => match.toLowerCase());
+                        elem.value = elem.value.replace(/((?:^|\s))./gi, (match) => match.toUpperCase());
+                }
+                //инпут "ваше сообщение - только кирилица и запятые"
+                if (elem.getAttribute('name') === 'user_message') {
+                        elem.value = elem.value.replace(/([^а-я- ]|-$|^-)/gi, '');
+                        elem.value = elem.value.replace(/-{2,}/g, '-');
+                        elem.value = elem.value.replace(/\s{2,}/g, ' ').trim();
+                }
+
+                //интупы "email" - только латиница и спецсимволы
+                if (elem.getAttribute('name') === 'user_email') {
+                        elem.value = elem.value.replace(/([^a-z@-_!~*'\.]|-$|^-)/gi, '');
+                        elem.value = elem.value.replace(/\s{2,}/, ' ');
+                        elem.value = elem.value.replace(/-{2,}/g, '-').trim();
+                }
+                //инпуты "номер телефона"
+                if (elem.getAttribute('name') === 'user_phone') {
+                        elem.value = elem.value.replace(/([^\d()-]|-$|^-)/gi, '');
+                        elem.value = elem.value.replace(/\s{2,}/, '');
+                        elem.value = elem.value.replace(/-{2,}/g, '-');
+                        elem.value = elem.value.replace(/^(- )$/g, '').trim();
+                }
+                
+            });
+        });
+    };
+    inputsСhange();
+
+
+    // наша команда (смена фотографий по наведению мыши)
+    const ourTeam = () => {
+        const container = document.querySelectorAll('.row')[8];
+        container.addEventListener('mouseover', (event) => {
+            let target = event.target;
+            if (target.classList.contains('command__photo')) {
+                target.src = target.dataset.img;
+            }
+        });
+    };
+    ourTeam();
+
     });
