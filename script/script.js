efc843a1f91e316d15cb37160e38a37c9f72d1e4
Vartan Bagdasarov <vartanbagdasarov@mail.ru>
2021-08-27 15:52:47 +0300

    ВыполниУ задание №24

--- script/script.js
+++ script/script.js
@@ -294,6 +294,55 @@ window.addEventListener('DOMContentLoaded', function () {
     };
     slider();
 
+    //калькулятор
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total'),
+            calcCount = document.querySelector('.calc-count');
+        
+        const countSum = () => {
+            let total = 0;
+            let countValue = 1,
+                dayValue =1;
+            const typeValue = +calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            
+            
+            
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+            if (calcDay && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+            
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            } else {
+                total = 0;
+            }
+            
+
+            
+
+
+
+            totalValue.textContent = total;
+        };
+        
+        calcBlock.addEventListener('change', (event) => {
+            let target = event.target;
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
 
     //отслеживание ввода в инпуты
     const inputsСhange = () => {
