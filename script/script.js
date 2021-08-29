0067cc14a86563d0f2a8b57ffe70d015b5118238
Vartan Bagdasarov <vartanbagdasarov@mail.ru>
2021-08-30 02:11:25 +0300

    Добавил валидатор для инпутов

--- script/script.js
+++ script/script.js
@@ -126,10 +126,6 @@ window.addEventListener('DOMContentLoaded', function () {
                 });
             });
 
-            popupClose.addEventListener('click', () => {
-                popup.style.display = 'none';
-            });
-
             popup.addEventListener('click', (event) => {
                 let target = event.target;
 
@@ -352,46 +348,46 @@ window.addEventListener('DOMContentLoaded', function () {
     calc(100);
 
     //отслеживание ввода в инпуты
-    const inputsСhange = () => {
-        const allInputs = [...document.getElementsByTagName('input')];
-        allInputs.forEach((elem) => {
-            elem.addEventListener('blur', () => { 
-            //инпут "Ваше имя" - только кирилица и запятые, первая буква каждого слова - заглавная, удалены все лишние пробелы и точки
-                if (elem.getAttribute('name') === 'user_name') {
-                        elem.value = elem.value.replace(/ {2,}/g, ' ');
-                        elem.value = elem.value.replace(/-{2,}/g, '-');
-                        elem.value = elem.value.replace(/(-$|^-)/g, '-');
-                        elem.value = elem.value.replace(/[а-я]/gi, (match) => match.toLowerCase());
-                        elem.value = elem.value.replace(/((?:^|\s))./g, (match) => ` ${match.toUpperCase()}`).trim();
-                }
-                //инпут "ваше сообщение - только кирилица и запятые"
-                if (elem.getAttribute('name') === 'user_message') {
-                        elem.value = elem.value.replace(/([^а-я- ]|-$|^-)/gi, '');
-                        elem.value = elem.value.replace(/-{2,}/g, '-');
-                        elem.value = elem.value.replace(/\s{2,}/g, ' ').trim();
-                }
+    // const inputsСhange = () => {
+    //     const allInputs = [...document.getElementsByTagName('input')];
+    //     allInputs.forEach((elem) => {
+    //         elem.addEventListener('blur', () => { 
+    //         //инпут "Ваше имя" - только кирилица и запятые, первая буква каждого слова - заглавная, удалены все лишние пробелы и точки
+    //             if (elem.getAttribute('name') === 'user_name') {
+    //                     elem.value = elem.value.replace(/ {2,}/g, ' ');
+    //                     elem.value = elem.value.replace(/(-{2,}|-$|^-)/g, '-');
+    //                     elem.value = elem.value.replace(/([^а-я])/g, '');
+    //                     elem.value = elem.value.replace(/[а-я]/gi, (match) => match.toLowerCase());
+    //                     elem.value = elem.value.replace(/((?:^|\s))./g, (match) => ` ${match.toUpperCase()}`).trim();
+    //             }
+    //             //инпут "ваше сообщение - только кирилица и запятые"
+    //             if (elem.getAttribute('name') === 'user_message') {
+    //                     elem.value = elem.value.replace(/([^а-я- ]|-$|^-)/gi, '');
+    //                     elem.value = elem.value.replace(/-{2,}/g, '-');
+    //                     elem.value = elem.value.replace(/\s{2,}/g, ' ').trim();
+    //             }
 
-                //интупы "email" - только латиница и спецсимволы
-                if (elem.getAttribute('name') === 'user_email') {
-                        elem.value = elem.value.replace(/([^a-z@-_!~*'\.]|-$|^-)/gi, '');
-                        elem.value = elem.value.replace(/\s{2,}/, ' ');
-                        elem.value = elem.value.replace(/-{2,}/g, '-').trim();
-                }
-                //инпуты "номер телефона"
-                if (elem.getAttribute('name') === 'user_phone') {
-                        elem.value = elem.value.replace(/([^\d()-]|-$|^-)/gi, '');
-                        elem.value = elem.value.replace(/\s{2,}/, '');
-                        elem.value = elem.value.replace(/-{2,}/g, '-');
-                        elem.value = elem.value.replace(/^(- )$/g, '').trim();
-                }
-                if (elem.classList.contains('calc-item')) {
-                    elem.value = elem.value.replace(/[^0-9]/g, '');
-                }
+    //             //интупы "email" - только латиница и спецсимволы
+    //             if (elem.getAttribute('name') === 'user_email') {
+    //                     elem.value = elem.value.replace(/([^a-z@-_!~*'\.]|-$|^-)/gi, '');
+    //                     elem.value = elem.value.replace(/\s{2,}/, ' ');
+    //                     elem.value = elem.value.replace(/-{2,}/g, '-').trim();
+    //             }
+    //             //инпуты "номер телефона"
+    //             if (elem.getAttribute('name') === 'user_phone') {
+    //                     elem.value = elem.value.replace(/([^\d()-]|-$|^-)/gi, '');
+    //                     elem.value = elem.value.replace(/\s{2,}/, '');
+    //                     elem.value = elem.value.replace(/-{2,}/g, '-');
+    //                     elem.value = elem.value.replace(/^(- )$/g, '').trim();
+    //             }
+    //             if (elem.classList.contains('calc-item')) {
+    //                 elem.value = elem.value.replace(/[^0-9]/g, '');
+    //             }
                 
-            });
-        });
-    };
-    inputsСhange();
+    //         });
+    //     });
+    // };
+    // inputsСhange();
 
 
     // наша команда (смена фотографий по наведению мыши)
